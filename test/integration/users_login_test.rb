9985b04dac636132d06adaec99fb29b96544978f
daichiccchi <e5khtamcx@i.softbank.jp>
2021-09-17 10:30:58 +0000

    Implement basic login

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid information followed by logout" do
+    get login_path  #ログイン用のパスを開く
+    post login_path, params: { session: {email: @user.email,
+                                         password: 'password' }} #セッション用パスに有効な情報をパスする
+    assert is_logged_in?
+    assert_redirected_to @user #リダイレクト先が正しいかどうかの確認
+    follow_redirect!           #リダイレクト先のページに移動する
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0 #ログイン用リンクが表示されていないことを確認
+    assert_select "a[href=?]", logout_path  #ログアウト用リンクが表示されていることを確認する
+    assert_select "a[href=?]", user_path(@user) #プロフィール用リンクが表示されていることを確認する
+    
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  # test "the truth" do
+  #   assert true
+  # en
+  test "login with valid email/invalid password" do #ログイン失敗時のメッセージのテスト
+    get login_path  #ログイン用のパスを開く
+    assert_template 'sessions/new'#フォームが正しく表示されたか
+    post login_path, params: { session:{ email: @user.email, password: "invalid"}}  #空のアドレスとパスワードを送信（セッション用パスにPOST）
+    assert_not is_logged_in?
+    assert_template "sessions/new" #セッションのページを再表示
+    assert_not flash.empty? #フラッシュメッセージが追加されているか
+    get root_path # 別のページにいったん移動する
+    assert flash.empty? # フラッシュメッセージが消えているか
+  end
+end
